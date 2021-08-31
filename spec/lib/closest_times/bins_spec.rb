f234e9249d9909d43656281468435689439511ba
David Milanese <milanese.david@gmail.com>
2021-09-01 01:24:04 +0930

    feat: add service to seperate array into bins

---
+++ spec/lib/closest_times/bins_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'closest_times/bins'
+
+RSpec.describe ClosestTimes::Bins do
+  describe '#call' do
+    it { expect(described_class.new([]).call).to eq [[]] }
+    it { expect(described_class.new([1]).call).to eq [[1]] }
+    it { expect(described_class.new([1, 2]).call).to eq [[1, 2]] }
+    it { expect(described_class.new([1, 2, 3]).call).to eq [[1, 2, 3]] }
+    it { expect(described_class.new([1, 2, 10]).call).to eq [[1, 2], [10]] }
+    it { expect(described_class.new([1, 2, 10, 11]).call).to eq [[1, 2], [10, 11]] }
+    it { expect(described_class.new([1, 2, 10, 11, 11]).call).to eq [[1, 2], [10, 11, 11]] }
+    it { expect(described_class.new([10, 11, 2, 1, 11]).call).to eq [[1, 2], [10, 11, 11]] }
+    it { expect(described_class.new([1, 2, 10, 11, 20, 25]).call).to eq [[1, 2], [10, 11], [20, 25]] }
+    it { expect(described_class.new([1, 2, 10, 100]).call).to eq [[1, 2], [10], [100]] }
+  end
+end
