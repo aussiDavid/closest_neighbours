b2a797f017431ca845ca2d8da65c9c62ad84319e
David Milanese <milanese.david@gmail.com>
2021-09-02 01:42:09 +0930

    feat:  add service to split the largests bin in a list of bins

---
+++ spec/lib/closest_times/divide_max_bin_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'closest_times/divide_max_bin'
+
+RSpec.describe ClosestTimes::DivideMaxBin do
+  describe '#call' do
+    it { expect(described_class.new([[]]).call).to eq [[]] }
+    it { expect(described_class.new([[1, 2]]).call).to eq [[1], [2]] }
+    it { expect(described_class.new([[1, 2, 3, 4]]).call).to eq [[1, 2], [3, 4]] }
+    it { expect(described_class.new([[1, 2, 3]]).call).to eq [[1, 2], [3]] }
+    it { expect(described_class.new([[1, 2], [3]]).call).to eq [[1], [2], [3]] }
+    it { expect(described_class.new([[1], [2, 3]]).call).to eq [[1], [2], [3]] }
+    it { expect(described_class.new([[1], [2, 3], [4, 5]]).call).to eq [[1], [2], [3], [4, 5]] }
+    it { expect(described_class.new([[1], []]).call).to eq [[1], []] }
+  end
+end
