83983a97538a81012acb9256493ee13987795aec
David Milanese <milanese.david@gmail.com>
2021-09-03 02:25:39 +0930

    docs: add yardoc

--- lib/closest_times.rb
+++ lib/closest_times.rb
@@ -13,10 +13,26 @@ require 'closest_times/errors/non_integer_groups_error'
 
 # Takes an array of elements, groups them by their ordered closest neighbors into n groups
 module ClosestTimes
+  #
+  # Match array of times up into groups that are the closest to each other
+  #
+  # @param [Integer] groups The number of groups to create
+  # @param [Array|Set|Enumerable] data The data set of items to group
+  #
+  # @return [Array] An array of `groups` groups
+  #
   def self.times(groups, data)
     Times.new(groups, data).call
   end
 
+  #
+  # Match array of times up into groups that are the closest to each other
+  #
+  # @param [Integer] groups The number of groups to create
+  # @param [Array|Set|Enumerable] data The data set of items to group
+  #
+  # @return [Array] An array of `groups` groups
+  #
   def self.ordered(groups, data)
     Ordered.new(groups, data).call
   end
