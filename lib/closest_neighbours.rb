1756768181165cd93edd9504639342b7b883b01c
David Milanese <milanese.david@gmail.com>
2021-09-15 14:51:49 +0930

    feat: remove unused algorithm

--- lib/closest_neighbours.rb
+++ lib/closest_neighbours.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 require 'closest_neighbours/version'
-
-# Services
-require 'closest_neighbours/times'
 require 'closest_neighbours/ordered'
 
 # Errors
@@ -14,19 +11,12 @@ require 'closest_neighbours/errors/non_integer_groups_error'
 # Takes an array of elements, groups them by their ordered closest neighbors into n groups
 module ClosestNeighbours
   #
-  # Match array of times up into groups that are the closest to each other
+  # Split an Enumerable into specified number of groups containing the closest elements in each group.
   #
-  # @param [Integer] groups The number of groups to create
-  # @param [Array|Set|Enumerable] data The data set of items to group
+  # @example
   #
-  # @return [Array] An array of `groups` groups
-  #
-  def self.times(groups, data)
-    Times.new(groups, data).call
-  end
-
-  #
-  # Match array of times up into groups that are the closest to each other
+  #   ClosestNeighbours.ordered(2, [10, 2, 11, 5])
+  #   # => [[2, 5], [10, 11]]
   #
   # @param [Integer] groups The number of groups to create
   # @param [Array|Set|Enumerable] data The data set of items to group
--- spec/closest_neighbours_spec.rb
+++ spec/closest_neighbours_spec.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 RSpec.describe ClosestNeighbours do
   it { expect(described_class::VERSION).not_to be nil }
-  it { expect(described_class.times(1, [])).not_to be nil }
   it { expect(described_class.ordered(1, [])).not_to be nil }
 end
