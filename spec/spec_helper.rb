661ac903667e1f82acb78e1bd4ea15771feb5cac
David Milanese <milanese.david@gmail.com>
2021-09-15 13:28:15 +0930

    chore: remove duplicate code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,22 +18,6 @@ end
 
 require 'closest_times'
 
-SimpleCov::Formatter::LcovFormatter.config do |c|
-  c.report_with_single_file = true
-  c.output_directory = 'coverage'
-  c.lcov_file_name = 'lcov.info'
-end
-
-SimpleCov.start do
-  enable_coverage :branch
-  formatter SimpleCov::Formatter::MultiFormatter.new([
-                                                       SimpleCov::Formatter::LcovFormatter,
-                                                       SimpleCov::Formatter::HTMLFormatter
-                                                     ])
-end
-
-require 'closest_times'
-
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
